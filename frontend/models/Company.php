756faba243877c47250a12a587b46a9292c54089
xindalu <14706931@qq.com>
2018-03-13 01:52:43 +0000

    init

---
+++ frontend/models/Company.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+ *
+ * @property int $id
+ * @property string $company_name
+ * @property string $description
+ */
+class Company extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'company';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['company_name'], 'string', 'max' => 100],
+            [['description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'company_name' => 'Company Name',
+            'description' => 'Description',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return CompanyQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CompanyQuery(get_called_class());
+    }
+}
